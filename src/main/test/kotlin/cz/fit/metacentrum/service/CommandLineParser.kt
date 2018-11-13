1a225c6bbcba2a9ddbc4e00dd4df2f7c2f72fe0e
Jakub Tucek <jakub.tucek@gmail.com>
2018-11-13 23:36:07 +0100

    Add Test internal prefix

--- src/main/test/kotlin/cz/fit/metacentrum/service/CommandLineParser.kt
+++ src/main/test/kotlin/cz/fit/metacentrum/service/CommandLineParser.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 
-class CommandLineParserTest {
+internal class CommandLineParserTest {
 
     @Test
     fun testThatInputIsParsedCorrectly() {
