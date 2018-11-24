c2d0c4447646dc98e0e8885484490e7ef752ddba
Jakub Tucek <jakub.tucek@gmail.com>
2018-11-24 22:46:31 +0100

    Add copy matlab files implementation

--- src/main/kotlin/cz/fit/metacentrum/service/executor/CopyMatlabFilesExecutor.kt
+++ src/main/kotlin/cz/fit/metacentrum/service/executor/CopyMatlabFilesExecutor.kt
@@ -3,15 +3,24 @@ package cz.fit.metacentrum.service.executor
 import cz.fit.metacentrum.domain.ExecutionMetadata
 import cz.fit.metacentrum.domain.MatlabTaskType
 import cz.fit.metacentrum.service.api.TaskExecutor
+import java.nio.file.Files
+import java.nio.file.Paths
 
 /**
  *
  * @author Jakub Tucek
  */
 class CopyMatlabFilesExecutor : TaskExecutor {
+
     override fun execute(metadata: ExecutionMetadata): ExecutionMetadata {
         val matlabAction = metadata.configFile.taskType as MatlabTaskType
 
+        val outPath = metadata.executionOutputPath ?: throw IllegalStateException("Execution path not set")
+
+        Files.copy(
+                Paths.get(matlabAction.folder),
+                outPath.resolve("matlab")
+        )
 
         return metadata
     }
