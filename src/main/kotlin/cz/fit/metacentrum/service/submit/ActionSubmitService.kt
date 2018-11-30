364e651beebd0df0695ebdceb0c5628f41a8779a
Jakub Tucek <jakub.tucek@gmail.com>
2018-11-30 23:16:13 +0100

    Add cleanup on fail

--- src/main/kotlin/cz/fit/metacentrum/service/submit/ActionSubmitService.kt
+++ src/main/kotlin/cz/fit/metacentrum/service/submit/ActionSubmitService.kt
@@ -9,7 +9,11 @@ import cz.fit.metacentrum.service.api.TaskExecutor
 import cz.fit.metacentrum.service.input.SerializationService
 import cz.fit.metacentrum.service.input.validator.ConfigValidationService
 import cz.fit.metacentrum.util.ConsoleWriter
+import cz.fit.metacentrum.util.FileUtils
+import mu.KotlinLogging
 import javax.inject.Inject
+
+private val logger = KotlinLogging.logger {}
 
 /**
  *
@@ -29,18 +33,14 @@ class ActionSubmitService() : ActionService<ActionSubmit> {
         when (config.taskType) {
             is MatlabTaskType -> runExecutors(config, matlabExecutors)
         }
-    }
 
+    }
 
     private fun runExecutors(config: ConfigFile, executorSet: Set<TaskExecutor>) {
         val initMetadata = ExecutionMetadata(configFile = config)
         ConsoleWriter.writeDelimiter()
         val finalMetadata = executorSet.asSequence()
-                .fold(initMetadata) { metadata, executor ->
-                    val res = executor.execute(metadata)
-                    ConsoleWriter.writeStatusDone()
-                    res
-                }
+                .fold(initMetadata) { metadata, executor -> safelyExecute(metadata, executor) }
         ConsoleWriter.writeDelimiter()
         serializationService.persistMetadata(finalMetadata)
     }
@@ -57,5 +57,26 @@ class ActionSubmitService() : ActionService<ActionSubmit> {
             System.exit(1)
         }
         return parsedConfig
+    }
+
+    private fun safelyExecute(metadata: ExecutionMetadata, executor: TaskExecutor): ExecutionMetadata {
+        try {
+            val res = executor.execute(metadata)
+            ConsoleWriter.writeStatusDone()
+            return res
+        } catch (e: Exception) {
+            logger.error("Executor in submit service failed. Cleaning up")
+            cleanup(metadata)
+            throw e
+        }
+    }
+
+    private fun cleanup(metadata: ExecutionMetadata) {
+        if (metadata.storagePath != null) {
+            FileUtils.deleteFolder(metadata.storagePath)
+        }
+        if (metadata.metadataStoragePath != null) {
+            FileUtils.deleteFolder(metadata.metadataStoragePath)
+        }
     }
 }
