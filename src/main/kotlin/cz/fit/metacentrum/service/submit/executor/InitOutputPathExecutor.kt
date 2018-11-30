b3461c5bd83998ee03eef532888ffdbf3d8ffafb
Jakub Tucek <jakub.tucek@gmail.com>
2018-11-30 23:08:39 +0100

    Fix init output path

--- src/main/kotlin/cz/fit/metacentrum/service/submit/executor/InitOutputPathExecutor.kt
+++ src/main/kotlin/cz/fit/metacentrum/service/submit/executor/InitOutputPathExecutor.kt
@@ -1,5 +1,6 @@
 package cz.fit.metacentrum.service.submit.executor
 
+import cz.fit.metacentrum.domain.config.ConfigEnvironment
 import cz.fit.metacentrum.domain.meta.ExecutionMetadata
 import cz.fit.metacentrum.service.api.TaskExecutor
 import cz.fit.metacentrum.util.ConsoleWriter
@@ -7,6 +8,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.time.format.DateTimeFormatter
+import kotlin.streams.toList
 
 /**
  *
@@ -14,15 +16,47 @@ import java.time.format.DateTimeFormatter
  */
 class InitOutputPathExecutor : TaskExecutor {
     override fun execute(metadata: ExecutionMetadata): ExecutionMetadata {
-        val newName = metadata.timestamp.format(
+        val formattedTimestamp = metadata.timestamp.format(
                 DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss")
         )
-        ConsoleWriter.writeStatus("Initializing output directories under name $newName")
+        val newName = getNewName(metadata.configFile.environment, formattedTimestamp)
+
+        ConsoleWriter.writeStatus("Initializing output directories under name $formattedTimestamp")
 
         val storagePath = initPath(newName, metadata.storagePath, metadata.configFile.environment.storagePath)
         val metadataStoragePath = initPath(newName, metadata.metadataStoragePath, metadata.configFile.environment.metadataStoragePath)
 
         return metadata.copy(storagePath = storagePath, metadataStoragePath = metadataStoragePath)
+    }
+
+    /**
+     * Checks storage path and finds last task folder name. Parses order from folder name, add 1 and uses it as new name.
+     */
+    private fun getNewName(environment: ConfigEnvironment, formattedTimestamp: String): String {
+        val storagePath = Paths.get(environment.storagePath)
+
+        // TODO: Move to constant
+        val taskFolderPrefix = "task-"
+        val taskFolderRegex = """^task-([0-9]+).*$""".toRegex()
+        // default new name
+        var newName = "${taskFolderPrefix}1"
+        if (Files.exists(storagePath)) {
+            val highestFolderNumber: Int? = Files.list(storagePath)
+                    .filter { Files.isDirectory(it) && it.fileName.toString().matches(taskFolderRegex) }
+                    .map {
+                        val (order) = taskFolderRegex.find(it.fileName.toString())!!.destructured
+                        order.toInt()
+                    }
+                    .toList()
+                    .filterNotNull()
+                    .sortedDescending()
+                    .firstOrNull()
+            if (highestFolderNumber != null) {
+                newName = "${taskFolderPrefix}_${highestFolderNumber + 1}"
+            }
+        }
+
+        return "$newName-$formattedTimestamp"
     }
 
     private fun initPath(folderName: String, pathFromMetadata: Path?, configPath: String): Path {
