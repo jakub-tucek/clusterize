0c816abeb2aa989cb2830eb5973e581747dc18df
Jakub Tucek <jakub.tucek@gmail.com>
2018-12-01 16:23:42 +0100

    Read username via whoami

--- src/main/kotlin/cz/fit/metacentrum/service/submit/executor/UsernameResolverExecutor.kt
+++ src/main/kotlin/cz/fit/metacentrum/service/submit/executor/UsernameResolverExecutor.kt
@@ -1,16 +1,30 @@
 package cz.fit.metacentrum.service.submit.executor
 
+import com.google.inject.Inject
 import cz.fit.metacentrum.domain.meta.ExecutionMetadata
+import cz.fit.metacentrum.service.api.ShellService
 import cz.fit.metacentrum.service.api.TaskExecutor
+import mu.KotlinLogging
+
+
+private val logger = KotlinLogging.logger { }
 
 /**
  *
  * @author Jakub Tucek
  */
 class UsernameResolverExecutor : TaskExecutor {
+
+    @Inject
+    private lateinit var shellService: ShellService
+
     override fun execute(metadata: ExecutionMetadata): ExecutionMetadata {
+        val (output, status, errOutput) = shellService.runCommand("whoami")
+        if (status != 0) {
+            logger.info("Retrieving username failed with status ${status}. ${errOutput}")
+        }
         return metadata.copy(
-                submittingUsername = System.getProperty("user.name")
+                submittingUsername = output
         )
     }
 
