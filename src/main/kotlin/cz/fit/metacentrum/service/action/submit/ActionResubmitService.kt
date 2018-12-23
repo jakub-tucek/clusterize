4552f560ec11260b980ef485a0150dc9161bc812
Jakub Tucek <jakub.tucek@gmail.com>
2018-12-23 10:06:11 +0100

    Add reading resubmit stuff

--- src/main/kotlin/cz/fit/metacentrum/service/action/submit/ActionResubmitService.kt
+++ src/main/kotlin/cz/fit/metacentrum/service/action/submit/ActionResubmitService.kt
@@ -3,10 +3,12 @@ package cz.fit.metacentrum.service.action.submit
 import com.google.inject.name.Named
 import cz.fit.metacentrum.config.actionResubmitMatlabExecutorsTokens
 import cz.fit.metacentrum.domain.ActionResubmit
+import cz.fit.metacentrum.domain.meta.ExecutionMetadata
 import cz.fit.metacentrum.service.SubmitRunner
 import cz.fit.metacentrum.service.api.ActionService
 import cz.fit.metacentrum.service.api.TaskExecutor
 import cz.fit.metacentrum.service.input.SerializationService
+import java.nio.file.Paths
 import javax.inject.Inject
 
 /**
@@ -25,10 +27,38 @@ class ActionResubmitService : ActionService<ActionResubmit> {
     private lateinit var serializationService: SerializationService
 
     override fun processAction(argumentAction: ActionResubmit) {
-        val (taskId, metadataStoragePath) = argumentAction
+        val metadata = readTaskMetadata(argumentAction)
+
+
+
 //        val resubmitMetadata = argumentAction.metadata.copy(rerun = true)
 //        when (resubmitMetadata.configFile.taskType) {
 //            is MatlabTaskType -> submitRunner.run(resubmitMetadata, matlabResubmitExecutors)
 //        }
+    }
+
+
+    private fun readTaskMetadata(argumentAction: ActionResubmit): ExecutionMetadata {
+        val (taskId, metadataStoragePath) = argumentAction
+        val appConfiguration = serializationService.parseMetadataIdPathMapping(
+                Paths.get(metadataStoragePath)
+        )
+        val taskIdInt = taskId.toIntOrNull()
+        if (taskIdInt == null) {
+            println("Given task identification is not proper number")
+            System.exit(1)
+        }
+        val taskPath = appConfiguration?.idToPathMap?.get(taskIdInt)
+        if (taskPath == null) {
+            println("Given path was not found in folder. Maybe you did not take number from last list command?")
+            System.exit(1)
+        }
+        val taskMetadata = serializationService.parseMetadata(Paths.get(taskPath))
+        if (taskMetadata == null) {
+            println("Path $taskPath does not contain valid metadata file.")
+            System.exit(1)
+        }
+
+        return taskMetadata!!
     }
 }
