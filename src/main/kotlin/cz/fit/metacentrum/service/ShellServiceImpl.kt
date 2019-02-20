48baf1384ae686d1d71e9c9efb06a4fdcb4e98ff
Jakub Tucek <jakub.tucek@gmail.com>
2019-02-20 19:18:01 +0100

    Add waitFor

--- src/main/kotlin/cz/fit/metacentrum/service/ShellServiceImpl.kt
+++ src/main/kotlin/cz/fit/metacentrum/service/ShellServiceImpl.kt
@@ -1,8 +1,11 @@
 package cz.fit.metacentrum.service
 
+import cz.fit.metacentrum.config.appName
 import cz.fit.metacentrum.domain.CommandOutput
 import cz.fit.metacentrum.service.api.ShellService
 import mu.KotlinLogging
+import java.nio.file.Files
+import java.util.concurrent.TimeUnit
 
 private val logger = KotlinLogging.logger { }
 
@@ -25,16 +28,32 @@ class ShellServiceImpl : ShellService {
      * Runs commands in console with 5s timeout in /bin/sh. Read text are trimmed.
      */
     override fun runCommand(command: String): CommandOutput {
+        val out = Files.createTempFile(appName, "command-out")
+        val outErr = Files.createTempFile(appName, "command-out")
+        Files.deleteIfExists(out)
+        Files.deleteIfExists(outErr)
+        Files.createFile(out)
+        Files.createFile(outErr)
+
         val process = ProcessBuilder("/bin/sh", "-c", command)
+                .redirectError(outErr.toFile())
+                .redirectOutput(out.toFile())
                 .start()
 
-        val errInput = process.errorStream.bufferedReader().use { it.readText() }.trim()
-        val input = process.inputStream.bufferedReader().use { it.readText() }.trim()
+        process.waitFor(60, TimeUnit.SECONDS)
+        if (process.isAlive) {
+            logger.debug { "Process did not end. Finishing" }
+            return CommandOutput("", 146, "")
+        }
 
-
-        return CommandOutput(
-                input, process.exitValue(), errInput
+        val commandOutput = CommandOutput(
+                Files.readAllBytes(out).toString(),
+                process.exitValue(),
+                Files.readAllBytes(outErr).toString()
         )
+        Files.delete(out)
+        Files.delete(outErr)
+        return commandOutput
     }
 
 }
