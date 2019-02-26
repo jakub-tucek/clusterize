9640af916d01d6adca4c9d64f5d2ba4ae2c4eb9a
Jakub Tucek <jakub.tucek@gmail.com>
2019-02-26 17:26:04 +0100

    Add job creation time to track queue time

--- src/main/kotlin/cz/fit/metacentrum/domain/meta/ExecutionMetadataJob.kt
+++ src/main/kotlin/cz/fit/metacentrum/domain/meta/ExecutionMetadataJob.kt
@@ -1,6 +1,7 @@
 package cz.fit.metacentrum.domain.meta
 
 import java.nio.file.Path
+import java.time.LocalDateTime
 
 
 // Job that is or was submitted.
@@ -8,5 +9,6 @@ data class ExecutionMetadataJob(val jobPath: Path, // run path where output is s
                                 val jobId: Int, // identical to iteration combination index
                                 val jobInfo: JobInfo, // Additional info about job
                                 val resubmitCounter: Int = 0,
-                                val jobParent: ExecutionMetadataJob? = null
+                                val creationTime: LocalDateTime = LocalDateTime.now(),
+                                val jobParent: ExecutionMetadataJob? = null // prev run,
 )
