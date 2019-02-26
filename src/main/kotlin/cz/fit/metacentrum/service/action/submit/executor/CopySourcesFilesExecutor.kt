e8f652787241e7eb70f3a1af24288f2f0e0df7e4
Jakub Tucek <jakub.tucek@gmail.com>
2019-02-26 22:08:48 +0100

    Dont copy sources

--- src/main/kotlin/cz/fit/metacentrum/service/action/submit/executor/CopySourcesFilesExecutor.kt
+++ src/main/kotlin/cz/fit/metacentrum/service/action/submit/executor/CopySourcesFilesExecutor.kt
@@ -1,10 +1,8 @@
 package cz.fit.metacentrum.service.action.submit.executor
 
-import cz.fit.metacentrum.config.FileNames
 import cz.fit.metacentrum.domain.meta.ExecutionMetadata
 import cz.fit.metacentrum.service.api.TaskExecutor
 import cz.fit.metacentrum.util.ConsoleWriter
-import cz.fit.metacentrum.util.FileUtils
 import java.nio.file.Paths
 
 /**
@@ -14,17 +12,10 @@ import java.nio.file.Paths
 class CopySourcesFilesExecutor : TaskExecutor {
 
     override fun execute(metadata: ExecutionMetadata): ExecutionMetadata {
-        ConsoleWriter.writeStatus("Copying sources files from ${metadata.configFile.general.sourcesPath}")
+        ConsoleWriter.writeStatus("Preparing sources files from ${metadata.configFile.general.sourcesPath}")
 
-        val outPath = metadata.paths.metadataStoragePath ?: throw IllegalStateException("MetadataStorage path not set")
-        val sourcesOutPath = outPath.resolve(FileNames.sourcesFolder)
 
-        FileUtils.copyDirectory(
-                Paths.get(metadata.configFile.general.sourcesPath),
-                outPath.resolve(FileNames.sourcesFolder)
-        )
-
-        return metadata.copy(paths = metadata.paths.copy(sourcesPath = sourcesOutPath))
+        return metadata.copy(paths = metadata.paths.copy(sourcesPath = Paths.get(metadata.configFile.general.sourcesPath)))
     }
 
 }
